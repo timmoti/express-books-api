9ec823ca6105436443e0e2dc08f8317a31cbe003
David Tan <davified@GMAIL.COM>
2018-07-25 11:46:40 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.json({ message: "hello express-blog-api" });
+  res.json({ message: "hello express-books-api" });
 });
 
 module.exports = router;
